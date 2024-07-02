a1159861fcb8ae277fffb294304022fcee94d7b1
MateuszApps <mateusz.lukowski3@gmail.com>
2024-07-02 15:44:02 +0200

    init the project

---
+++ src/projects.js
@@ -0,0 +1,15 @@
+class Project {
+
+    constructor(name, description, dueDate){
+        this.name = name;
+        this.description = description;
+        this.dueDate = dueDate;
+    }
+}
+
+
+function addNewProject(name) {
+    const content = document.getElementById('canvas');
+
+    
+}
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'development', // Set the mode to development
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+  },
+};
